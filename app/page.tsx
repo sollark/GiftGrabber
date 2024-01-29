6906ceeb3a1b930ce2e0aefad2af0e0f997489d1
Andrey Solonikov <andreysol@yahoo.com>
2024-01-29 20:55:09 +0200

    Refactor home page and add full-screen layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section className='full-screen flex align-center justify-center'>
+      <h1>Create an event</h1>
+    </section>
+  )
 }
 
 export default Home
