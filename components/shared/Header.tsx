18cc6cba7dc7e3a13b62393c8170b41835b84ee0
Andrey Solonikov <andreysol@yahoo.com>
2024-01-30 17:22:20 +0200

    Add Footer and Header components

---
+++ components/shared/Header.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+const Header: FC = () => {
+  return (
+    <header
+      style={{
+        paddingBlock: '.5rem',
+        paddingInline: '2rem',
+      }}>
+      <h1>GIFT GRABBER</h1>
+    </header>
+  )
+}
+
+export default Header
