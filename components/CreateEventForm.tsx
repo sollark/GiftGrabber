3a0b709e08c9458b0bd9584fef4b5ed2362bdaf0
Andrey Solonikov <andreysol@yahoo.com>
2024-02-04 16:20:41 +0200

    Add html-react-parser and display table in CreateEventForm

--- components/CreateEventForm.tsx
+++ components/CreateEventForm.tsx
@@ -1,14 +1,15 @@
 'use client'
 
+import { convertExcelToJson } from '@/utils/excelToJson'
 import { EventSchema } from '@/utils/z/schema'
+import parse from 'html-react-parser'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
+import * as XLSX from 'xlsx'
 import ControlledFileInput from './form/ControlledFileInput'
 import ControlledTextInput from './form/ControlledTextInput'
 import ErrorMessage from './form/ErrorMessage'
 import Form from './form/Form'
-import { convertExcelToJson } from '@/utils/excelToJson'
-import * as XLSX from 'xlsx'
 
 const defaultValues = {
   eventName: '',
@@ -21,6 +22,7 @@ const CreateEventForm = () => {
   const [eventName, setEventName] = useState('')
   const [eventEmail, setEventEmail] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [table, setTable] = useState<ReactNode>()
 
   const handleSubmit = async (data: any) => {
     console.log('handleSubmit', data)
@@ -31,8 +33,25 @@ const CreateEventForm = () => {
     /* generate and display HTML */
     const workbook = XLSX.read(await data.eventFile.arrayBuffer())
     const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-    const table = XLSX.utils.sheet_to_html(worksheet)
-    console.log('handleSubmit table', table)
+    const html = XLSX.utils.sheet_to_html(worksheet)
+
+    // Parse the HTML string into a DOM object
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(html, 'text/html')
+
+    // Extract the table element
+    const table = doc.querySelector('table')
+
+    if (!table) {
+      console.error('No table found in the worksheet')
+      return
+    }
+
+    // Convert the table element to a HTML string
+    const tableHtml = table ? table.outerHTML : '<div></div>'
+
+    const reactTable = parse(tableHtml)
+    setTable(reactTable)
 
     // const newEvent = createEvent()
     // router.push(`/`)
@@ -65,6 +84,7 @@ const CreateEventForm = () => {
         inputProps={{ style: { fontSize: 24 } }}
       />
       <ErrorMessage message={errorMessage} />
+      <div>{table}</div>
     </Form>
   )
 }
