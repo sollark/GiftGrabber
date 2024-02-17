2e5bb6a271753c328da2c9e4733819f3a8b80447
Andrey Solonikov <andreysol@yahoo.com>
2024-02-17 11:20:26 +0200

    Add table to display applicant details

--- app/(root)/events/[eventId]/[ownerId]/page.tsx
+++ app/(root)/events/[eventId]/[ownerId]/page.tsx
@@ -1,4 +1,6 @@
 import { getEventDetails } from '@/app/actions/event.action'
+import { Gift } from '@/database/models/gift.model'
+import { Person } from '@/database/models/person.model'
 import { FC } from 'react'
 
 type SearchParamProps = {
@@ -17,6 +19,28 @@ const EventDetails: FC<SearchParamProps> = async ({
   return (
     <div>
       <h1>Event Details</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Is Grabbed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {event.applicantList.map((applicant: Person) => (
+            <tr key={applicant._id}>
+              <td>{`${applicant.firstName} ${applicant.lastName}`}</td>
+              <td>
+                {event.giftList.find(
+                  (gift: Gift) => gift.owner._id === applicant._id
+                )
+                  ? 'Yes'
+                  : 'No'}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>{' '}
     </div>
   )
 }
--- components/PeopleList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const PeopleList = () => {
-  return <div>PeopleList</div>
-}
-
-export default PeopleList
