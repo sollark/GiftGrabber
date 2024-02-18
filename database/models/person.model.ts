c3a5409b9b74e262a27ad88bb5a4872363381b8b
Andrey Solonikov <andreysol@yahoo.com>
2024-02-18 19:11:36 +0200

    Remove commented out code and unused import in person.model.ts

--- database/models/person.model.ts
+++ database/models/person.model.ts
@@ -1,29 +1,19 @@
 import { Schema, Types, model, models } from 'mongoose'
-import { Order } from './order.model'
 
 export type Person = {
-  _id: string
   firstName: string
   lastName: string
-  // orders: Order[] | []
 }
 
 type PersonDoc = {
   _id: Types.ObjectId
   firstName: string
   lastName: string
-  // orders: Types.ObjectId[] | []
 }
 
 const personSchema: Schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  // orders: [
-  //   {
-  //     type: Types.ObjectId,
-  //     ref: 'Order',
-  //   },
-  // ],
 })
 
 const PersonModel = models.Person || model<PersonDoc>('Person', personSchema)
