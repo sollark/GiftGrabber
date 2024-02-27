d8f08dea01bbacb16e1826b0c9663e167e97f04a
Andrey Solonikov <andreysol@yahoo.com>
2024-02-27 19:51:46 +0200

    Add approverList field to Order model

--- database/models/order.model.ts
+++ database/models/order.model.ts
@@ -5,7 +5,7 @@ import { Gift } from './gift.model'
 export type Order = {
   _id: Types.ObjectId
   createdAt: Date
-  eventId: Types.ObjectId
+  approverList: Person[]
   applicant: Person
   gifts: Gift[]
   orderId: string
@@ -16,7 +16,7 @@ export type Order = {
 type OrderDoc = {
   _id: Types.ObjectId
   createdAt: Date
-  eventId: Types.ObjectId
+
   applicant: Types.ObjectId
   gifts: Types.ObjectId[]
   orderId: string
@@ -27,7 +27,7 @@ type OrderDoc = {
 
 const orderSchema: Schema = new Schema({
   createdAt: { type: Date, default: Date.now },
-  eventId: { type: Types.ObjectId, ref: 'Event', required: true },
+  approverList: [{ type: Types.ObjectId, ref: 'Person' }],
   applicant: { type: Types.ObjectId, ref: 'Person', required: true },
   gifts: [{ type: Types.ObjectId, ref: 'Gift', required: true }],
   orderId: { type: String, required: true },
