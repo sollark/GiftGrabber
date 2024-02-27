cad9f6a3754830e96e52fd0c16aa5b730e76cb28
Andrey Solonikov <andreysol@yahoo.com>
2024-02-27 18:12:59 +0200

    Add eventId field to Order model

--- database/models/order.model.ts
+++ database/models/order.model.ts
@@ -5,6 +5,7 @@ import { Gift } from './gift.model'
 export type Order = {
   _id: Types.ObjectId
   createdAt: Date
+  eventId: Types.ObjectId
   applicant: Person
   gifts: Gift[]
   orderId: string
@@ -15,6 +16,7 @@ export type Order = {
 type OrderDoc = {
   _id: Types.ObjectId
   createdAt: Date
+  eventId: Types.ObjectId
   applicant: Types.ObjectId
   gifts: Types.ObjectId[]
   orderId: string
@@ -25,6 +27,7 @@ type OrderDoc = {
 
 const orderSchema: Schema = new Schema({
   createdAt: { type: Date, default: Date.now },
+  eventId: { type: Types.ObjectId, ref: 'Event', required: true },
   applicant: { type: Types.ObjectId, ref: 'Person', required: true },
   gifts: [{ type: Types.ObjectId, ref: 'Gift', required: true }],
   orderId: { type: String, required: true },
