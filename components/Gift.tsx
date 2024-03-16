90f198c995d8428325656166573eb4d9e43abd35
Andrey Solonikov <andreysol@yahoo.com>
2024-03-16 15:45:06 +0200

    Add Gift component

---
+++ components/Gift.tsx
@@ -0,0 +1,17 @@
+import { Gift } from '@/database/models/gift.model'
+import { FC } from 'react'
+
+type GiftProps = {
+  gift: Gift
+}
+
+const Gift: FC<GiftProps> = ({ gift }) => {
+  if (!gift) return <></>
+
+  const { owner, receiver } = gift
+  const giftStatus = receiver === null ? 'Available' : 'Claimed'
+
+  return <p>{`${owner.firstName} ${owner.lastName}: ${giftStatus}`}</p>
+}
+
+export default Gift
