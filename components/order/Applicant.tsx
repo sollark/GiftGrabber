46e05bb6ec904dd1a6306473cf4e661224a35db7
Andrey Solonikov <andreysol@yahoo.com>
2024-03-22 07:39:38 +0200

    Add onChangePerson event handler to PersonAutocomplete

--- components/order/Applicant.tsx
+++ components/order/Applicant.tsx
@@ -37,6 +37,7 @@ const Applicant = () => {
     <PersonAutocomplete
       peopleList={applicantList}
       onSelectPerson={onSelectApplicant}
+      onChangePerson={(person) => {}}
     />
   )
 }
